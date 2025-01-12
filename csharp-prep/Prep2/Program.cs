3e4e759337c6feeba8be464a052be34dd2224b3e
Kevin Stout <kevinjstout975@gmail.com>
2025-01-11 17:15:55 -0700

    W01 Prove part 2, grade percent to letter program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string percentString = Console.ReadLine();
+        int percent = int.Parse(percentString);
+
+        string letter;
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You've passed, you should feel proud!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. You can get it next time, we believe in you!");
+        }
+
     }
 }
