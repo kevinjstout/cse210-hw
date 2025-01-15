046025f41bc072dbe117e382ae379a3adbee61d2
Kevin Stout <kevinjstout975@gmail.com>
2025-01-15 14:56:00 -0700

    W02 part 4, list of numbers with calculations

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        float input = -1;
+
+        List<float> list = new List<float>();
+
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                list.Add(input);
+            }
+        }
+
+        float sum = 0;
+        foreach (float i in list)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = sum / (list.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        float largest = list[0];
+        foreach (float i in list)
+        {
+            if (i >= largest)
+            {
+                largest = i;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
