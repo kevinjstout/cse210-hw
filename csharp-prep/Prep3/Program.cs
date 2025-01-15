2693fbbcfe219fdf04e55394acd5d66a4b56cf4f
Kevin Stout <kevinjstout975@gmail.com>
2025-01-14 18:40:53 -0700

    W02, part 3 - magic number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool unguessed = true;
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        while (unguessed)
+        {
+            Console.Write("What is your guess? ");
+            float guess = float.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                unguessed = false;
+            }
+        }
+
     }
 }
