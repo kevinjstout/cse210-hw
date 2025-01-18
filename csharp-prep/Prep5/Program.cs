91badce936b7578c87de9f1791dbd63a9c08f04f
Kevin Stout <kevinjstout975@gmail.com>
2025-01-17 17:28:39 -0700

    W02 part 5, functions (fav num, name, etc)

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static float PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            float number = float.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static float SquareNumber(float n)
+        {
+            return n*n;
+        }
+
+        static void DisplayResult(string name, float square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}.");
+        }
+
+
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        float number = PromptUserNumber();
+
+        float square = SquareNumber(number);
+        
+        DisplayResult(name, square);
+
+    
     }
 }
