a380383143974bacf87cff97ac890f433eb37253
Kevin Stout <kevinjstout975@gmail.com>
2025-01-27 15:55:13 -0700

    stuff

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,20 @@
+using System.Net.Mime;
+
+public class Entry
+{
+    public string _content;
+
+    public string _prompt;
+
+    public string _date;
+
+    public string PrettyText()
+    {
+        return '';
+    }
+
+    public string GeneratePrompt()
+    {
+        
+    }
+}
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,4 +1,15 @@
 public class Journal
 {
+    public string _path;
 
+    public string _currentEntry;
+
+    public void Open()
+    {
+        
+    }
+    public void Save()
+    {
+        
+    }
 }
