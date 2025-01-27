884fdf0ac0f4500dc8bdcf105368d7d33f007d61
Kevin Stout <kevinjstout975@gmail.com>
2025-01-27 15:45:52 -0700

    wrote some more

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -1,16 +1,35 @@
 public class Menu
 {
-    public List<string> _selections;
+    //public List<string> _selection; - do i even need this?
     public int _input;
 
 
-    public void Display()
+    public int DisplayAndPrompt()
     {
+        Console.WriteLine("Hello. Please make a selection based on what you'd like to do. (Enter an integer.)");
+        Console.WriteLine("1. Get prompt.");
+        Console.WriteLine("2. Open journal.");
+        Console.WriteLine("3. Save prompt.");
 
+        int selection = int.Parse(Console.ReadLine());
+        return selection;
     }
 
     public void ExecuteSelection()
     {
-        
+        if (_input == 1)
+        {
+            //call entry to generate and return string
+        }
+
+        if (_input == 2)
+        {
+            //call file, get text, print
+        }
+
+        if (_input == 3)
+        {
+            //call journal class to save current prompt
+        }
     }
 }
