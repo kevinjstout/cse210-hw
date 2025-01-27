b411dd5eb800241ed4d5a496ceab84473dd4c513
Kevin Stout <kevinjstout975@gmail.com>
2025-01-27 15:34:07 -0700

    Created menu class

---
+++ prove/Develop02/Menu.cs
@@ -0,0 +1,16 @@
+public class Menu
+{
+    public List<string> _selections;
+    public int _input;
+
+
+    public void Display()
+    {
+
+    }
+
+    public void ExecuteSelection()
+    {
+        
+    }
+}
