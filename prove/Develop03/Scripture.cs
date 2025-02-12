444f8dba8a74218d9cbe31c5e547a5a4d6425754
Kevin Stout <kevinjstout975@gmail.com>
2025-02-12 11:49:38 -0700

    worked on the class' basics

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,4 +1,10 @@
 public class Scripture
 {
+    private Reference _reference = new();
+
+    private List<Word> _scripture = new List<Word>();
+
+    private List<int> _remainingWords = new List<int>();
+
     
 }
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,4 +1,21 @@
 public class Word
 {
+    private int _length;
+
+    private string _word;
+
+    public void Word()
+    {
+        
+    }
     
+    public int GetLength()
+    {
+
+    }
+
+    public string GetWord()
+    {
+
+    }
 }
