38a3a3ae117f44020f407025e2db32bc9e7534de
Kevin Stout <kevinjstout975@gmail.com>
2025-02-18 17:22:26 -0700

    Finished and tested whole thing.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment test = new("John Homeworkdoer", "Fractions", "Section 42", "18-19");
+
+        Console.WriteLine(test.GetSummary());
+
+        Console.WriteLine(test.GetHomeworkList());
+
+
+    
+        WritingAssignment test2 = new("John Homeworkdoer", "Fractions", "the revolution of spaghetti");
+
+        Console.WriteLine(test2.GetSummary());
+
+        Console.WriteLine(test2.GetWritingInformation());
     }
 }
