25b0c8ebf60a21940a0b352f2008a5a9c9c0af6b
Kevin Stout <kevinjstout975@gmail.com>
2025-03-05 14:47:34 -0700

    finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s = new Square("blue", 2);
+        Circle c = new Circle("red", 2);
+        Rectangle r = new Rectangle("yellow", 4, 8);
+
+        List<Shape> list = new List<Shape> {s, c, r};
+
+        foreach (Shape shape in list)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
